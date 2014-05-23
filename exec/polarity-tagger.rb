26fb37c41aa5169a8dc5ac23ea76d2e6157e6e2f
Yorick Peterse <yorickpeterse@olery.com>
2014-05-23 16:01:33 +0200

    Implemented resource switcher for daemon/Puma.

--- exec/polarity-tagger.rb
+++ exec/polarity-tagger.rb
@@ -1,9 +1,19 @@
 #!/usr/bin/env ruby
 
 require 'opener/daemons'
+require 'opener/core/resource_switcher'
+
 require_relative '../lib/opener/polarity_tagger'
 
-options = Opener::Daemons::OptParser.parse!(ARGV)
+switcher      = Opener::Core::ResourceSwitcher.new
+switcher_opts = {}
+
+parser = Opener::Daemons::OptParser.new do |opts|
+  switcher.bind(opts, switcher_opts)
+end
+
+options = parser.parse!(ARGV)
 daemon  = Opener::Daemons::Daemon.new(Opener::PolarityTagger, options)
 
+switcher.install(switcher_opts)
 daemon.start
